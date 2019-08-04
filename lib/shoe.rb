9405639cdc03375b1983ae6397fc4940282190e7
Oscar Shoudt <shoudto@gmail.com>
2019-08-04 22:47:36 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,8 @@
 class Shoe
 
-  attr_accessor :brand, :color, :size, :material, :condition 
+  attr_accessor :brand, :color, :size, :material, :condition
 
   def initialize(brand)
     @brand = brand 
-  end 
+  end
 end
